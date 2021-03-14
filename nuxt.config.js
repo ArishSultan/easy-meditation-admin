fc59d69fd04eef8f80d8981dfca71ce27e300b8e
arish <arishkhan28.08@gmail.com>
2021-03-14 18:55:08 +0500

    port fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,82 +1,82 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: "%s - " + process.env.npm_package_name,
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
-  css: ['~/assets/css/style.css'],
+  css: ["~/assets/css/style.css"],
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/vue-composition-api', '~/plugins/vue-the-mask'],
+  plugins: ["~/plugins/vue-composition-api", "~/plugins/vue-the-mask"],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
-    '@nuxtjs/vuetify'
+    "@nuxtjs/stylelint-module",
+    "@nuxtjs/vuetify"
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/auth',
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    "@nuxtjs/auth",
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa"
   ],
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'http://localhost:4000'
+    baseURL: "http://localhost:4000"
   },
   router: {
-    middleware: ['auth', 'check-scope']
+    middleware: ["auth", "check-scope"]
   },
   auth: {
     redirect: {
-      login: '/auth/sign-in',
-      logout: '/',
-      callback: '/auth/sign-in',
-      home: '/'
+      login: "/auth/sign-in",
+      logout: "/",
+      callback: "/auth/sign-in",
+      home: "/"
     },
     strategies: {
       local: {
         endpoints: {
           login: {
-            url: '/auth/sign-in',
-            method: 'post',
-            propertyName: 'access_token'
+            url: "/auth/sign-in",
+            method: "post",
+            propertyName: "access_token"
           },
-          logout: { url: '/auth/sign-out', method: 'post' },
-          user: { url: '/auth/profile', method: 'get', propertyName: 'user' }
+          logout: { url: "/auth/sign-out", method: "post" },
+          user: { url: "/auth/profile", method: "get", propertyName: "user" }
         }
       }
     }
@@ -86,7 +86,7 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       themes: {
         dark: {
@@ -112,7 +112,7 @@ export default {
   },
 
   server: {
-    port: 8000, // default: 3000
-    host: '0.0.0.0' // default: localhost
+    port: 8080, // default: 3000
+    host: "0.0.0.0" // default: localhost
   }
-}
+};
